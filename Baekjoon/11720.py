58bba99c1dd341effa32a24f6850b8941b4e613b
sooyeon lee <sooyeonlee127@gmail.com>
2022-10-21 14:56:33 +0900

    TIL

---
+++ Baekjoon/11720.py
@@ -0,0 +1,6 @@
+N = int(input())
+num = input()
+result = 0
+for i in num :
+    result += int(i)
+print(result)
---
+++ Baekjoon/2884.py
@@ -0,0 +1,11 @@
+time = input().split(' ')
+h = int(time[0])
+m = int(time[1]) - 45
+
+if m < 0  :
+    m += 60
+    h -= 1
+    if h < 0 :
+        h += 24
+
+print(h, m)
