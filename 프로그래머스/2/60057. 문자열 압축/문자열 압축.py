89da75215bb6a910d6f30af676cc0af12cb96fbe
sooyeonlee <110149875+sooyeonlee127@users.noreply.github.com>
2023-11-30 22:30:03 +0900

    [level 2] Title: 문자열 압축, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,21 @@
+def solution(s):
+    answer = len(s)
+    for n in range(1, len(s)//2+1):
+        result = ''
+        temp = s[:n]
+        cnt = 1
+        for i in range(n, len(s), n):
+            if temp == s[i:i+n]:
+                cnt+=1
+            else:
+                if cnt != 1:
+                    result += str(cnt)
+                result += temp
+                temp = s[i:i+n]
+                cnt = 1
+        if cnt != 1:
+            result += str(cnt)
+        result +=temp
+        answer = min(len(result), answer)
+        
+    return answer
