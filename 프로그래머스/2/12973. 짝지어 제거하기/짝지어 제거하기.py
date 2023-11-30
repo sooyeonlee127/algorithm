a5098ad2318c543084d9a7b4c265c96c17cfb760
sooyeonlee <110149875+sooyeonlee127@users.noreply.github.com>
2023-11-30 21:56:21 +0900

    [level 2] Title: 짝지어 제거하기, Time: 136.16 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    stack = []
+    for letter in s:
+        if len(stack) == 0:
+            stack.append(letter)
+        else:
+            if stack[-1] == letter:
+                stack.pop()
+            else:
+                stack.append(letter)
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
