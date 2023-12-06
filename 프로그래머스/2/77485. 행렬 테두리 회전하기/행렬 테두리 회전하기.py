6e0125317c3d8bd7860f2395fbd96d3ec8079572
sooyeonlee <110149875+sooyeonlee127@users.noreply.github.com>
2023-12-06 10:06:06 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 140.03 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -4,35 +4,36 @@
 
 def solution(rows, columns, queries):
     answer = []
-    MAP = [[(i)*columns+(j+1) for j in range(columns)] for i in range(rows)]
+    arr = []
     num = 1
-
-    for x1,y1,x2,y2 in queries:
-        MIN = 21e5
+    for row in range(rows):
+        temp = []
+        for col in range(columns):
+            temp.append(num)
+            num+=1
+        arr.append(temp)
+    for x1, y1, x2, y2 in queries:
         x1 -= 1
         y1 -= 1
         x2 -= 1
         y2 -= 1
-        backup = MAP[x1][y1]
-        MIN = min(MIN, backup)
-        for i in range(x1,x2):
-            MAP[i][y1] = MAP[i+1][y1]
-            MIN = min(MIN, MAP[i+1][y1])
-            
-        for i in range(y1, y2):
-            MAP[x2][i] = MAP[x2][i+1]
-            MIN = min(MIN, MAP[x2][i+1])
-            
-        for i in range(x2, x1, -1):
-            MAP[i][y2] = MAP[i-1][y2]
-            MIN = min(MIN, MAP[i-1][y2])
-            
-        for i in range(y2, y1, -1):
-            MAP[x1][i] = MAP[x1][i-1]
-            MIN = min(MIN, MAP[x1][i-1])
-            
-
-        MAP[x1][y1+1] = backup
+        backup = arr[x1][y1]
+        MIN = backup
+        for i in range(x1, x2):
+            arr[i][y1] = arr[i+1][y1]
+            MIN = min(MIN, arr[i+1][y1])
+        for j in range(y1, y2):
+            arr[x2][j] = arr[x2][j+1]
+            MIN = min(MIN, arr[x2][j+1])
+        for i in range(x2,x1,-1):
+            arr[i][y2] = arr[i-1][y2]
+            MIN = min(MIN, arr[i-1][y2])
+        for j in range(y2, y1+1, -1):
+            arr[x1][j] = arr[x1][j-1]
+            MIN = min(MIN, arr[x1][j-1])
+        arr[x1][y1+1] = backup
         answer.append(MIN)
+
     
+        
     return answer
